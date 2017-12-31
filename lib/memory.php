021252f1c688a65514ed0172e689dc8953ae02ac
UndisputedBasement33 <34984201+UndisputedBasement33@users.noreply.github.com>
2017-12-31 15:35:52 +0100

    Update memory.php
    with raspbian stretch the memory usage wasn't shown. 'free -mo' istn't a valid option for 'free'. Also the calculation of used ram in percentage ist't correct anymore.

--- lib/memory.php
+++ lib/memory.php
@@ -14,20 +14,20 @@ class Memory {
 
         $result = array();
 
-        $out = $ssh->exec_noauth('free -mo');
+        $out = $ssh->exec_noauth('free -mw');
         preg_match_all('/\s+([0-9]+)/', $out[1], $matches);
-        list($total, $used, $free, $shared, $buffers, $cached) = $matches[1];
+        list($total, $used, $free, $shared, $buffers, $cached, $available) = $matches[1];
 
         $ramDetails = $ssh->shell_exec_noauth('ps -e -o pmem,user,args --sort=-pmem | sed "/^ 0.0 /d" | head -' . self::$DETAIL_LINE_COUNT);
 
-        $result['percentage'] = round(($used - $buffers - $cached) / $total * 100);
+        $result['percentage'] = round($available / $total * 100);
         if ($result['percentage'] >= '80')
             $result['alert'] = 'warning';
         else
             $result['alert'] = 'success';
 
-        $result['free'] = $free + $buffers + $cached;
-        $result['used'] = $used - $buffers - $cached;
+        $result['free'] = $available;
+        $result['used'] = $used;
         $result['total'] = $total;
         $result['detail'] = $ramDetails;
 
@@ -38,7 +38,7 @@ class Memory {
         global $ssh;
         $result = array();
 
-        $out = $ssh->exec_noauth('free -mo');
+        $out = $ssh->exec_noauth('free -mw');
         preg_match_all('/\s+([0-9]+)/', $out[2], $matches);
         list($total, $used, $free) = $matches[1];
 
